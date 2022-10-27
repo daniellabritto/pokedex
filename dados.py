b97a021cf229d8bd93a9c4901084127028e97b21
daniellabritto <113472051+daniellabritto@users.noreply.github.com>
2022-10-27 15:46:49 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,57 @@
+pokemon = {
+
+    'Charmander' : {
+        'status' : ["HP: 39","Ataque: 52","Defesa: 43","Velocidade: 65","Total: 309"],
+        'habilidades': ["Blaze", "Solar Power"],
+        'tipo' : ['#004', 'Fogo', 'pokemon/imagens/004.png', '#F4B186'],
+    },
+
+    'Eevee' : {
+        'status' : ["HP: 55","Ataque: 55","Defesa: 50","Velocidade: 55","Total: 325"],
+        'habilidades': ["fuga", 'Antencipação de Adaptabilidade'],
+        'tipo' : ['#133', 'Normal', 'pokemon/imagens/133.png', '#CFA374'],
+    },
+
+    'Mimikyu' : {
+        'status' : ["HP: 55 ","Ataque: 90","Defesa:80","Velocidade: 96","Total: 476"],
+        'habilidades': ["Disfarce", 'trans'],
+        'tipo' : ['#778', 'Fantasma', 'pokemon/imagens/778.png', '#D6D0AC'],
+    },
+    
+    'Psyduck' : {
+        'status' : ["HP: 50 ","Ataque: 52","Defesa:48","Velocidade: 65","Total: 320"],
+        'habilidades': ["Úmido", "Cloud Nine"],
+        'tipo' : ['#054', 'Agua', 'pokemon/imagens/054.png','#E0BF70'],
+    },
+
+    'Pumpkaboo' : {
+        'status' : ["HP: 49","Ataque: 66","Defesa: 70","Velocidade: 51","Total: 335"],
+        'habilidades': ["Captação","Insomnia"],
+        'tipo' : ['#710', 'Fantasma', 'pokemon/imagens/710.png', '#E0997B'],
+    },
+
+    'Togepi' : {
+        'status' : ["HP: 35","Ataque: 20","Defesa: 65","Velocidade: 20","Total: 245"],
+        'habilidades': ["Hustle","Serene Grace"],
+        'tipo' : ['#175', 'Fada', 'pokemon/imagens/175.png','#55A8C6'],
+    },
+
+    'Mewtwo' : {
+        'status' : ["HP: 180","Ataque: 110","Defesa: 90","Velocidade: 130","Total: 680"],
+        'habilidades': ["Pressão Nervosa","insomnia"],
+        'tipo' : ['#175', 'Psíquico', 'pokemon/imagens/150.png','#B58DB2'],
+    },
+
+    'Bulbassauro' : {
+        'status' : ["HP: 45","Ataque: 49","Defesa: 49","Velocidade: 45","Total: 318"],
+        'habilidades': ["Excesso de Clorofila","Chicotada Verde"],
+        'tipo' : ['#175', 'Grama', 'pokemon/imagens/001.png','#85C090'],
+    },
+
+    'Pikachu' : {
+        'status' : ["HP: 35","Ataque: 55","Defesa: 40","Velocidade: 90","Total: 320"],
+        'habilidades': ["Raios Estáticos","Bola Elétrica"],
+        'tipo' : ['#175', 'Elétrico', 'pokemon/imagens/025.png','#F2D780'],
+    },
+    
+}
